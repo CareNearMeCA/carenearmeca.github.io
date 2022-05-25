7bff2e8f65ad85b566f377c0058dca631b6415a2
irenecasado <irenecs@stanford.edu>
2022-05-25 13:24:14 -0700

    css

--- mapFunction.js
+++ mapFunction.js
@@ -118,7 +118,7 @@
                     'PSYCHC',
                     '#db6ff2',
                     'RES',
-                    '#750c8c',
+                    '#6495ED',
                     'RES-DETOX',
                     '#1CC14E',
                     '#FF0000'
